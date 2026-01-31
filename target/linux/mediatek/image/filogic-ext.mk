61c3a2d5f5fad3751c8e366bd66312f245d7d206
chasey-dev <ellenyoung0912@gmail.com>
2026-01-31 22:21:22 +0800

    mediatek: filogic: image: re-arrange ext file by alphabetical order
    Signed-off-by: chasey-dev <ellenyoung0912@gmail.com>

--- target/linux/mediatek/image/filogic-ext.mk
+++ target/linux/mediatek/image/filogic-ext.mk
@@ -1,37 +1,3 @@
-define Device/xiaomi_mi-router-ax3000t-mtkuboot
-  DEVICE_VENDOR := Xiaomi
-  DEVICE_MODEL := Mi Router AX3000T
-  DEVICE_VARIANT := (MTK U-Boot layout)
-  DEVICE_DTS := mt7981b-xiaomi-mi-router-ax3000t-mtkuboot
-  DEVICE_DTS_DIR := ../dts-ext
-  UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  IMAGE_SIZE := 114688k
-  KERNEL_IN_UBI := 1
-  IMAGES += factory.bin
-  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += xiaomi_mi-router-ax3000t-mtkuboot
-
-define Device/netcore_n60-pro-mtkuboot
-  DEVICE_VENDOR := Netcore
-  DEVICE_MODEL := N60 Pro
-  DEVICE_VARIANT := (MTK U-Boot layout)
-  DEVICE_DTS := mt7986a-netcore-n60-pro-mtkuboot
-  DEVICE_DTS_DIR := ../dts-ext
-  DEVICE_PACKAGES := kmod-usb3 automount kmod-usb-ledtrig-usbport 
-  UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  KERNEL_IN_UBI := 1
-  IMAGES += factory.bin
-  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += netcore_n60-pro-mtkuboot
-
 define Device/clx_s20p
   DEVICE_VENDOR := CLX
   DEVICE_MODEL := S20P
@@ -58,3 +24,37 @@ define Device/h3c_magic-nx30-pro-mtkuboot
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += h3c_magic-nx30-pro-mtkuboot
+
+define Device/netcore_n60-pro-mtkuboot
+  DEVICE_VENDOR := Netcore
+  DEVICE_MODEL := N60 Pro
+  DEVICE_VARIANT := (MTK U-Boot layout)
+  DEVICE_DTS := mt7986a-netcore-n60-pro-mtkuboot
+  DEVICE_DTS_DIR := ../dts-ext
+  DEVICE_PACKAGES := kmod-usb3 automount kmod-usb-ledtrig-usbport 
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += netcore_n60-pro-mtkuboot
+
+define Device/xiaomi_mi-router-ax3000t-mtkuboot
+  DEVICE_VENDOR := Xiaomi
+  DEVICE_MODEL := Mi Router AX3000T
+  DEVICE_VARIANT := (MTK U-Boot layout)
+  DEVICE_DTS := mt7981b-xiaomi-mi-router-ax3000t-mtkuboot
+  DEVICE_DTS_DIR := ../dts-ext
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 114688k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += xiaomi_mi-router-ax3000t-mtkuboot
