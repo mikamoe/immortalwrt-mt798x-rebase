56b9b2c8bd413798782afeca6c266ed142e10497
chasey-dev <ellenyoung0912@gmail.com>
2026-02-06 17:17:27 +0800

    mediatek: filogic: add MTK U-Boot layout support for Xiaomi WR30U (#10)
    - This variant is NOT compatible with stock or OpenWrt U-Boot layout.
    
    Signed-off-by: chasey-dev <ellenyoung0912@gmail.com>

--- target/linux/mediatek/image/filogic-ext.mk
+++ target/linux/mediatek/image/filogic-ext.mk
@@ -75,3 +75,20 @@ define Device/xiaomi_mi-router-ax3000t-mtkuboot
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += xiaomi_mi-router-ax3000t-mtkuboot
+
+define Device/xiaomi_mi-router-wr30u-mtkuboot
+  DEVICE_VENDOR := Xiaomi
+  DEVICE_MODEL := Mi Router WR30U
+  DEVICE_VARIANT := (MTK U-Boot layout)
+  DEVICE_DTS := mt7981b-xiaomi-mi-router-wr30u-mtkuboot
+  DEVICE_DTS_DIR := ../dts-ext
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 114688k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += xiaomi_mi-router-wr30u-mtkuboot
